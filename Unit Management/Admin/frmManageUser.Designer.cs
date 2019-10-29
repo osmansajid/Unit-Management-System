9d1cff1c02b54324987bf9c6fc25e1a57ded1e1a
osmansajid <42430976+osmansajid@users.noreply.github.com>
2019-10-29 21:32:30 +0600

    Add files via upload

---
+++ Unit Management/Admin/frmManageUser.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace Unit_Management.Admin
+{
+    partial class frmManageUser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFullname = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbRole = new System.Windows.Forms.ComboBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.colCode = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colUserName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colEmail = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colRoleID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colRole = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colFullName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cmbRole);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.btnSave);
+            this.groupBox1.Controls.Add(this.txtPassword);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtUserName);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtEmail);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtFullname);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(596, 123);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Full Name";
+            // 
+            // txtFullname
+            // 
+            this.txtFullname.Location = new System.Drawing.Point(79, 24);
+            this.txtFullname.Name = "txtFullname";
+            this.txtFullname.Size = new System.Drawing.Size(163, 20);
+            this.txtFullname.TabIndex = 1;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(319, 24);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(271, 20);
+            this.txtEmail.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(259, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Email";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(79, 50);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(163, 20);
+            this.txtUserName.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "User Name";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(319, 53);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(163, 20);
+            this.txtPassword.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(246, 56);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Password";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(488, 51);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(102, 43);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 79);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Role";
+            // 
+            // cmbRole
+            // 
+            this.cmbRole.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbRole.FormattingEnabled = true;
+            this.cmbRole.Location = new System.Drawing.Point(79, 79);
+            this.cmbRole.Name = "cmbRole";
+            this.cmbRole.Size = new System.Drawing.Size(163, 21);
+            this.cmbRole.TabIndex = 10;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colCode,
+            this.colFullName,
+            this.colUserName,
+            this.colEmail,
+            this.colRoleID,
+            this.colRole});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(12, 141);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(596, 248);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
+            // 
+            // colCode
+            // 
+            this.colCode.Text = "Code";
+            // 
+            // colUserName
+            // 
+            this.colUserName.Text = "User Name";
+            this.colUserName.Width = 150;
+            // 
+            // colEmail
+            // 
+            this.colEmail.Text = "Email";
+            this.colEmail.Width = 150;
+            // 
+            // colRoleID
+            // 
+            this.colRoleID.Text = "RoleID";
+            this.colRoleID.Width = 0;
+            // 
+            // colRole
+            // 
+            this.colRole.Text = "Role";
+            this.colRole.Width = 80;
+            // 
+            // colFullName
+            // 
+            this.colFullName.Text = "Name";
+            this.colFullName.Width = 150;
+            // 
+            // frmManageUser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 401);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmManageUser";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmManageUser";
+            this.Load += new System.EventHandler(this.frmManageUser_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFullname;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmbRole;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader colCode;
+        private System.Windows.Forms.ColumnHeader colUserName;
+        private System.Windows.Forms.ColumnHeader colEmail;
+        private System.Windows.Forms.ColumnHeader colRoleID;
+        private System.Windows.Forms.ColumnHeader colRole;
+        private System.Windows.Forms.ColumnHeader colFullName;
+    }
+}
---
+++ Unit Management/Admin/frmManageUser.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Unit_Management.Core;
+
+
+namespace Unit_Management.Admin
+{
+    public partial class frmManageUser : Form
+    {
+        public frmManageUser()
+        {
+            InitializeComponent();
+        }
+        private string SelectedUser = null;
+        private string[] userInfo = new string[5];
+        DataOperations DB = new DataOperations();
+        internal void setUserInfo(string[] userInfo)
+        {
+            this.userInfo = userInfo;
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, string> USER = new Dictionary<string, string>();
+            USER.Add("UNAME", DB.validateTextBox(txtUserName));
+            if (!String.IsNullOrEmpty(txtPassword.Text))
+            {
+                USER.Add("UPASS", DB.CreateMD5(DB.validateTextBox(txtPassword)));
+            }
+           
+            USER.Add("UEMAIL", DB.validateTextBox(txtEmail));
+            USER.Add("UFULL_NAME", DB.validateTextBox(txtFullname));
+            USER.Add("UROLE", DB.validateComBox(cmbRole));
+
+            if (btnSave.Text == "Save")
+            {
+                MessageBox.Show(DB.insert("SYSTEM_USER", USER));
+                Clear();
+            }
+            else
+            {
+                //Update
+
+                MessageBox.Show(DB.update("SYSTEM_USER", USER, "CODE='"+SelectedUser+"'"));
+                btnSave.Text = "Save";
+                Clear();
+            }
+            LoadListView();
+        }
+
+
+
+        private void Clear()
+        {
+            txtEmail.Clear();
+            txtFullname.Clear();
+            txtPassword.Clear();
+            txtUserName.Clear();
+            SelectedUser = null;
+            btnSave.Text = "Save";
+            cmbRole.SelectedIndex = -1;
+        }
+        private void frmManageUser_Load(object sender, EventArgs e)
+        {
+            LoadRoleBox();
+            LoadListView();
+        }
+
+        private void LoadRoleBox()
+        {
+            DataTable DT = DB.select("SYSTEM_ROLE", "*");
+
+            cmbRole.DisplayMember = "NAME";
+            cmbRole.ValueMember = "CODE";
+            cmbRole.DataSource = DT;
+            cmbRole.SelectedIndex = -1;
+        }
+
+        private void LoadListView()
+        {
+            DataTable DT = DB.select("VW_SYSTEM_USER", "*");
+
+
+            if (DT.Rows.Count > 0)
+            {
+
+                listView1.BeginUpdate();
+                listView1.Items.Clear();
+
+                foreach (DataRow row in DT.Rows)
+                {
+                    ListViewItem item = new ListViewItem(row["CODE"].ToString());
+                    item.SubItems.Add(row["UFULL_NAME"].ToString());
+                    item.SubItems.Add(row["UNAME"].ToString());
+                    item.SubItems.Add(row["UEMAIL"].ToString());
+                    item.SubItems.Add(row["R_CODE"].ToString());
+                    item.SubItems.Add(row["NAME"].ToString());
+                    listView1.Items.Add(item);
+
+                }
+                listView1.EndUpdate();
+                listView1.Refresh();
+            }
+        }
+
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems != null)
+            {
+                ListViewItem item = listView1.SelectedItems[0];
+                txtEmail.Text = item.SubItems[colEmail.Index].Text;
+                txtFullname.Text = item.SubItems[colFullName.Index].Text;
+                txtUserName.Text = item.SubItems[colUserName.Index].Text;
+                SelectedUser = item.SubItems[colCode.Index].Text;
+                cmbRole.SelectedValue = item.SubItems[colRoleID.Index].Text;
+                btnSave.Text = "Update";
+            }
+        }
+    }
+}
